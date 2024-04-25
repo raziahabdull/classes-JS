a9f95b7df2f312a6f8226b1977b75101d5175609
Raziah <abdullahiraziah@gmail.com>
2024-04-25 08:05:35 +0300

    classes assignment

---
+++ classes.js
@@ -0,0 +1,55 @@
+function Car(make,nodel,year,availability){
+    this.make = "mazda".
+    this.model = "atenza".
+    this.year = 2021.
+    this.available = isAvailable.
+} 
+toggleAvailability(){
+    console.log(`The ${this.make} is not available for rent`)
+}
+
+const Car = new Car(make = "mazda",model= "atenza",year= 2021, available=isAvailable)
+
+
+function Rental(car,name,startdate,renteddate,srartDate,endDate){
+    this.rentalName = rentalName
+    this.startDate = startDate
+    this.rentedDate = renteddate
+    this.startDate = startDate
+    this.endDate = endDate
+}
+calculateRentalDuration(){
+    console.log(`This rental duration is ${this.endDate-this.startDate}`)
+}
+
+function Questions(text,options,answer,validAnswer){
+    this.text = text
+    this.options = options
+    this.answer = answer
+    this.validAnswer
+
+}
+checkAnswer(){
+    this.answer = answer
+}
+if (${this.answer} !== ${this.make}){
+    console.log(`The ${this.answer} is false`)
+}
+else (console.log(`true`));
+
+
+class Quiz{
+    constructor(answer,firstquestion,secondquestion)
+    this.firstquestion = question;
+    this.secondquestion = text;
+    this.submitanswer = answer;
+}
+checkAnswer(answer){
+    this.score = score
+}
+if (${this.answer}== "true"){
+    console.log(`${this.score}`)
+}
+else (console.log(0));
+
+
